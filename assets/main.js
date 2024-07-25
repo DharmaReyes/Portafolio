ac346d6fc0a1692fb41671fdc951bded21a2ba80
DharmaReyes <dharma.reyes323@gmail.com>
2024-07-24 22:17:33 -0400

    portafolio2

---
+++ assets/main.js
@@ -0,0 +1,29 @@
+function ShowMsg() {
+    var elements = document.getElementById("botonresume").style.display = "block";
+   
+}
+function ShowMsg() {
+    var element = document.getElementById("botonresume");
+    if (element.style.display === "none") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
+function ShowForm() {
+    var elements = document.getElementById("form").style.display = "block";
+}
+function ShowForm() {
+    var element = document.getElementById("form");
+    if (element.style.display === "none") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }}
+
+        function scrollToImages() {
+            document.getElementById('imagenes').scrollIntoView({ behavior: 'smooth' });
+        }
+    
+        
+        
